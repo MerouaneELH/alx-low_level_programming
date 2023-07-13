36edb513da639805b87a0d4ef590ec2ac0fbefa6
MerouaneELH <marwaneelhabchi1@gmail.com>
2023-07-13 07:39:36 -0700

    m

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - function that prints string
+ * Return: returns 0 when the  program  execute successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
