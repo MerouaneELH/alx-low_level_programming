3e5a764634b164ce0d5c023497f9f60c7d968013
MerouaneELH <136021607+MerouaneELH@users.noreply.github.com>
2023-07-14 15:16:06 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Prints alpha form a to z
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int i;
 
+	for (i = 48 ; i <= 57 ; i++){
+		putchar(i);
+    putchar(44);
+    putchar(30);
+  }
+	putchar('\n');
+	return (0);
+}
