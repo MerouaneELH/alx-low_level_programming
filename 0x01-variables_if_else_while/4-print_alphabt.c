5b850a53ae9610f5c60e83a1939f1a6e742e4ab7
MerouaneELH <136021607+MerouaneELH@users.noreply.github.com>
2023-07-14 14:41:28 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,9 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++){
-    if (letter != 'q' || letter != 'e')
-      putchar(letter);
-  }
+		if (letter != 'q' || letter != 'e')
+			putchar(letter);
+	}
 	putchar('\n');
 
 	return (0);
