50dcca2177209efdcd495d4f4eec6b58c63fa353
MerouaneELH <136021607+MerouaneELH@users.noreply.github.com>
2023-07-14 15:16:49 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 48 ; i <= 57 ; i++){
+	for (i = 48 ; i <= 57 ; i++)
+	{
 		putchar(i);
-    putchar(44);
-    putchar(30);
-  }
+		putchar(44);
+		putchar(32);
+	}
 	putchar('\n');
 	return (0);
 }
