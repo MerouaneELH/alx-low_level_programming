9e70beb66540b7c238fe2cc4e1a1305d7050d722
MerouaneELH <136021607+MerouaneELH@users.noreply.github.com>
2023-07-14 15:06:09 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Prints alpha form a to z
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int i;
+  
+  for (i = 48 ; i <= 57 ; i++)
+    putchar(i);
+	for (i = 97 ; i <= 102 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
