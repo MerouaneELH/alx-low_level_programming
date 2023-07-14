c4c21fb65df1a161663d5801bff7948b04d19953
MerouaneELH <136021607+MerouaneELH@users.noreply.github.com>
2023-07-14 14:54:32 +0100

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int nbr;
+
+	for (nbr = 0 ; nbr <= 9 ; nbr++)
+		putchar(nbr);
+
+	putchar('\n');
+
+	return (0);
+}
