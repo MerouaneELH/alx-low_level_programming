964ff0671676c81d2eacf1746ad506afc8375094
MerouaneELH <marwaneelhabchi1@gmail.com>
2023-07-14 05:26:38 -0700

    m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Prints alpha form a to z
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	for (int i = 97 ; i <= 122 ; i++)
+		putchar(i);
+	return (0);
+}
